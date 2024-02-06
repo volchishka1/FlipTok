f8a7289aafd0c65abfe5f903bdf136564d919b9c
macwolf <romazverb18@gmsil.com>
2024-02-07 00:03:04 +0300

    added searchScreenComponent

---
+++ src/components/searchScreen/searchScreenComponent.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+import React from 'react';
+
+export const SearchScreenComponent = () => {
+  return (
+    <View>
+      <Text>{'SearchScreen'}</Text>
+    </View>
+  );
+};
