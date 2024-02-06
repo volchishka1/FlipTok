dd25d24447a3a932d6dade470a458d59dfa497bf
macwolf <romazverb18@gmsil.com>
2024-02-07 00:03:55 +0300

    added navigator

---
+++ src/navigation/Navigator.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+
+import { AppStack } from '../stack/AppStack';
+
+export const Navigator = () => {
+  return (
+    <NavigationContainer>
+      <AppStack />
+    </NavigationContainer>
+  );
+};
