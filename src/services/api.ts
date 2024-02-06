dae71ff7287ded79ac532f6c09d6f6a510353f8e
macwolf <romazverb18@gmsil.com>
2024-02-07 00:02:08 +0300

    added api

---
+++ src/services/api.ts
@@ -0,0 +1,33 @@
+import { Alert } from 'react-native';
+
+import axios, { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
+
+import { HttpCode } from '../constants/const';
+
+const createAPI = (): AxiosInstance => {
+  const api = axios.create({
+    // baseURL: url,
+    baseURL: 'https://fliptok.app/api/fetch?url={tiktok',
+    // timeout: Number(process.env.REQUEST_TIMEOUT),
+  });
+
+  api.interceptors.response.use(
+    (response: AxiosResponse) => response,
+
+    (error: AxiosError) => {
+      const { response } = error;
+
+      if (response && response.status >= HttpCode.ServerError) {
+        Alert.alert(`server error: ${error.message}`);
+      } else if (response && response.status >= HttpCode.ClientError) {
+        Alert.alert(`client error: ${error.message}`);
+      } else {
+        Alert.alert(error.message);
+      }
+      return Promise.reject(error);
+    },
+  );
+  return api;
+};
+
+export const api = createAPI();
