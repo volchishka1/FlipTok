215899fe46f9082f753a405092be077604abf06a
macwolf <romazverb18@gmsil.com>
2024-02-07 00:02:42 +0300

    added const types

---
+++ src/constants/const.ts
@@ -0,0 +1,29 @@
+// export enum APIRoute {
+//   User = "/user",
+//   Avatar = "/avatar",
+//   AvatarUpdate = "/avatar/user",
+//   CreateAvatar = "/user/cteate/avatar",
+//   UpdateAvatar = "/avatar/update/avatarItem",
+//   Energy = "/checkIn/energy",
+//   LifeBalance = "/checkIn_count/percent",
+//   LastCheckIns = "/checkIn/last",
+//   CheckInCount = "/checkIn_count/count",
+//   CheckInPlaces = "/location/place/near",
+//   Location = "/checkIn/location",
+//   TypeLocation = "/typelocation",
+//   City = "/city",
+//   AvatarAbilities = "/avatar/stats",
+//   Wardrobe = "/user_item/item/user",
+//   WardrobeUpdateNew = "/user_item",
+//   PinnedItem = "/user_item"
+// }
+
+export enum HttpCode {
+  ServerError = 500,
+  ClientError = 400,
+}
+
+// export const SET_IS_AUTHORIZED = "SET_IS_AUTHORIZED";
+// export const NO_CHECK_IN_PLUG_TEXT = "No check-in points here";
+// export const ENERGY_IS_OVER = "Energy is over";
+// export const GEODATA_QUERY_INTERVAL = 120000;
