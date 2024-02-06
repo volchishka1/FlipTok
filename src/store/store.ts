aa61ecd3859b6a16050533c75ad2b8c2c173ef97
macwolf <romazverb18@gmsil.com>
2024-02-07 00:04:20 +0300

    added store

---
+++ src/store/store.ts
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import { api } from '../services/api';
+
+import { rootReducer } from './root-reducer';
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    }),
+});
