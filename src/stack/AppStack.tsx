45dba45c224f6c6ee332c437dd98d5f5c83dbf34
macwolf <romazverb18@gmsil.com>
2024-02-07 00:02:21 +0300

    added appStack component

---
+++ src/stack/AppStack.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { StartScreenComponent } from '../components/startScreen/startScreenComponent';
+import { ROUTES } from '../constants/routes';
+import { MainScreen } from '../tabs/MainScreen';
+
+export const AppStack = () => {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name={ROUTES.START_SCREEN} component={StartScreenComponent} />
+      <Stack.Screen name={ROUTES.MAIN_SCREEN} component={MainScreen} />
+    </Stack.Navigator>
+  );
+};
