e351a6b1ed1a337f5e4a3a357e4bb2311fa287ee
macwolf <romazverb18@gmsil.com>
2024-02-07 00:02:55 +0300

    added downloadScreenComponent

---
+++ src/components/downloadScreenComponent/downloadScreenComponent.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+import React from 'react';
+
+export const DownloadScreenComponent = () => {
+  return (
+    <View>
+      <Text>{'Hello app'}</Text>
+    </View>
+  );
+};
