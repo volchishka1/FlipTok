2e4c95e75faa40066402c65b19c1836126713fd3
macwolf <romazverb18@gmsil.com>
2024-02-08 23:49:38 +0300

    added icon and style to tabNavigator

--- src/tabs/MainScreen.tsx
+++ src/tabs/MainScreen.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import { FolderSvg } from '../../assets/folder';
+import { SearchSvg } from '../../assets/search';
 import { DownloadScreenComponent } from '../components/downloadScreenComponent/downloadScreenComponent';
 import { SearchScreenComponent } from '../components/searchScreen/searchScreenComponent';
 import { ROUTES } from '../constants/routes';
@@ -10,25 +12,30 @@ export const MainScreen = () => {
   const Tab = createBottomTabNavigator();
 
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: true,
+        tabBarLabelStyle: { color: '#6563ff', fontSize: 12 },
+      }}>
       <Tab.Screen
         name={ROUTES.SEARCH_SCREEN}
         component={SearchScreenComponent}
         options={{
-          tabBarLabel: '',
-          // tabBarIcon: ({ focused }) => {
-          //   return focused ? <SvgBottomHomeActive /> : <SvgBottomHome />;
-          // },
+          tabBarLabel: 'Search',
+          tabBarIcon: ({ focused }) => {
+            return <SearchSvg />;
+          },
         }}
       />
       <Tab.Screen
         name={ROUTES.DOWNLOAD_SCREEN}
         component={DownloadScreenComponent}
         options={{
-          tabBarLabel: '',
-          // tabBarIcon: (focused) => {
-          //   return <SvgBottomSearch />;
-          // },
+          tabBarLabel: 'Downloads',
+          tabBarIcon: (focused) => {
+            return <FolderSvg />;
+          },
         }}
       />
     </Tab.Navigator>
