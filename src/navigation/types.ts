a04522a43fa7f382ff37d473f3f337bbbf8c6d8c
macwolf <romazverb18@gmsil.com>
2024-02-08 23:47:19 +0300

    added routes types

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -3,4 +3,6 @@ import { ROUTES } from '../constants/routes';
 export type MainStackScreenNavigatorParamList = {
   [ROUTES.MAIN_SCREEN]: undefined;
   [ROUTES.START_SCREEN]: undefined;
+  [ROUTES.SEARCH_SCREEN]: undefined;
+  [ROUTES.DOWNLOAD_SCREEN]: undefined;
 };
