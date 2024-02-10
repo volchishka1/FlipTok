f58e192bbf97e70361ad7dbb787d33ac751ee724
macwolf <romazverb18@gmsil.com>
2024-02-10 21:47:02 +0300

    added loaderComponent

---
+++ src/components/startScreen/loaderComponent.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import LottieView from 'lottie-react-native';
+
+export const LoaderComponent = () => {
+  return (
+    <LottieView
+      source={require('../../../assets/animation_lkgrv9gp.json')}
+      autoPlay={true}
+      loop={true}
+      style={{
+        flex: 1,
+      }}
+    />
+  );
+};
