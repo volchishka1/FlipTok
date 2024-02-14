6c384cc949462fa7aa7cdc1951234d24df002d6c
macwolf <romazverb18@gmsil.com>
2024-02-14 21:59:37 +0300

    carried out some code in logo component

--- src/screens/mainScreen/mainScreenComponent.tsx
+++ src/screens/mainScreen/mainScreenComponent.tsx
@@ -1,8 +1,7 @@
-import { StatusBar, View } from 'react-native';
+import { View } from 'react-native';
 
 import React from 'react';
 
-import { LogoSvg } from '../../../assets/logo';
 import { MainScreen } from '../../tabs/MainScreen';
 
 import { mainScreenStyles } from './mainScreenStyles';
@@ -10,10 +9,6 @@ import { mainScreenStyles } from './mainScreenStyles';
 export const MainScreenComponent = () => {
   return (
     <View style={mainScreenStyles.rootContainer}>
-      <StatusBar backgroundColor={'#6f42c1'} />
-      <View style={mainScreenStyles.centerContainer}>
-        <LogoSvg />
-      </View>
       <MainScreen />
     </View>
   );
