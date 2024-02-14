85a142e2d6b1c32f61e1302af8b5a130c38e6f0b
macwolf <romazverb18@gmsil.com>
2024-02-14 21:46:36 +0300

    checked prettier and fix code style

---
+++ src/screens/termsAndConditionScreen/termsAndConditionsComponent.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from 'react-native';
+
+import React from 'react';
+
+export const TermsAndConditionComponent = () => {
+  let navigation;
+  return (
+    <View>
+      <Text>{'TERMSANDCONDITION'}</Text>
+    </View>
+  );
+};
