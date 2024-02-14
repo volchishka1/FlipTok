b5bb5f55040728376c0cc721efa4fa6591377b86
macwolf <romazverb18@gmsil.com>
2024-02-14 22:01:56 +0300

    fixed searchScreenComponent

--- src/screens/searchScreen/searchScreenComponent.tsx
+++ src/screens/searchScreen/searchScreenComponent.tsx
@@ -2,11 +2,14 @@ import { Text, View } from 'react-native';
 
 import React from 'react';
 
+import { LogoComponent } from '../../components/logoComponent/logoComponent';
+
 import { searchScreenStyles } from './searchScreenStyles';
 
 export const SearchScreenComponent = () => {
   return (
     <View style={searchScreenStyles.rootContainer}>
+      <LogoComponent />
       <Text style={{ color: 'white' }}>{'SearchScreen'}</Text>
     </View>
   );
