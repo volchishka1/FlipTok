77ae527b2227a93f762a3cb426495cc75fb1bdaf
macwolf <romazverb18@gmsil.com>
2024-02-16 17:04:22 +0300

    refactored babelConfig and buildGradle

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.android.js', '.ios.js', '.js', '.json', '.ts', '.tsx'],
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
