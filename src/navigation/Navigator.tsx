6ee60688f388dd3a0da138c2a99a4e9c5bab8ecc
macwolf <romazverb18@gmsil.com>
2024-02-16 17:02:25 +0300

    added gestureHandlerRootView and bottomSheetModalProvider in navigator

--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { NavigationContainer } from '@react-navigation/native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { MainStackNavigator } from '../stack/MainStack';
 
 export const Navigator = () => {
   return (
-    <NavigationContainer>
-      <MainStackNavigator />
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <BottomSheetModalProvider>
+        <NavigationContainer>
+          <MainStackNavigator />
+        </NavigationContainer>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
   );
 };
