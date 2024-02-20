ed386e4caa883cadd8e4c0f9bdc8f9e1d1f5c86c
macwolf <romazverb18@gmsil.com>
2024-02-20 12:36:09 +0300

    rename styles

--- src/screens/personalInfoScreen/styles.ts
+++ src/screens/personalInfoScreen/styles.ts
@@ -1,9 +1,9 @@
 import { StyleSheet, ViewStyle } from 'react-native';
 
-export type InfoScreenStyles = {
+export type Styles = {
   rootContainer: ViewStyle;
 };
 
-export const infoScreenStyles = StyleSheet.create<InfoScreenStyles>({
+export const infoScreenStyles = StyleSheet.create<Styles>({
   rootContainer: { backgroundColor: '#1d014b', flex: 1 },
 });
