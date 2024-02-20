df1fe660cb597d860d90e70df88985f5e355ca42
macwolf <romazverb18@gmsil.com>
2024-02-20 12:11:03 +0300

    refactor textInput

--- src/screens/searchScreen/searchScreenComponent.tsx
+++ src/screens/searchScreen/searchScreenComponent.tsx
@@ -1,17 +1,23 @@
 import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import { LogoComponent } from '../../components/logoComponent/logoComponent';
 
 import { searchScreenStyles } from './searchScreenStyles';
 
 export const SearchScreenComponent = () => {
+  const [value, setValue] = useState('');
   return (
     <View style={searchScreenStyles.rootContainer}>
       <LogoComponent />
       <View style={{ flexDirection: 'row', alignItems: 'center', marginHorizontal: '5%' }}>
-        <TextInput accessibilityLabel={'Text input field'} style={searchScreenStyles.input} />
+        <TextInput
+          value={value}
+          onChangeText={(text) => setValue(text)}
+          accessibilityLabel={'Text input field'}
+          style={searchScreenStyles.input}
+        />
         <TouchableOpacity style={searchScreenStyles.buttonStyle} accessibilityLabel={'button'}>
           <Text style={searchScreenStyles.textInput}>{'Скачать'}</Text>
         </TouchableOpacity>
