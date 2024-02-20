b9c7fd4a620aca1c44f90bc3da19de5845bdffe6
macwolf <romazverb18@gmsil.com>
2024-02-20 13:21:50 +0300

    added index to startScreen

---
+++ src/screens/startScreen/index.tsx
@@ -0,0 +1,27 @@
+import React, { FC, useEffect } from 'react';
+
+import { CompositeScreenProps } from '@react-navigation/native';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+
+import { ROUTES } from '../../constants/routes';
+import { MainStackScreenNavigatorParamList } from '../../navigation/types';
+
+import { StartScreenView } from './startScreenView';
+
+export type StartScreenComponentProps = CompositeScreenProps<
+  NativeStackScreenProps<MainStackScreenNavigatorParamList, ROUTES.MAIN_SCREEN>,
+  any
+>;
+
+export const StartScreen: FC<StartScreenComponentProps> = ({ navigation }) => {
+  const navigateToMainScreen = () => {
+    navigation.navigate(ROUTES.MAIN_SCREEN);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigateToMainScreen();
+    }, 9000);
+  });
+  return <StartScreenView />;
+};
