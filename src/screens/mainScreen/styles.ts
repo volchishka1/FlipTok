08147f92b6c91b431b1f39cf58e920fafa1dba42
macwolf <romazverb18@gmsil.com>
2024-02-20 12:44:24 +0300

    refactor styles mainScren

--- src/screens/mainScreen/styles.ts
+++ src/screens/mainScreen/styles.ts
@@ -1,9 +1,9 @@
 import { StyleSheet, ViewStyle } from 'react-native';
 
-export type MainScreenStyles = {
+export type Styles = {
   rootContainer: ViewStyle;
 };
 
-export const mainScreenStyles = StyleSheet.create<MainScreenStyles>({
+export const mainScreenStyles = StyleSheet.create<Styles>({
   rootContainer: { height: '100%', backgroundColor: '#1d014b' },
 });
