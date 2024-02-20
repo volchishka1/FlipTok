78c807625c1d81b997d57b2b83e199a8431a28a8
macwolf <romazverb18@gmsil.com>
2024-02-20 13:21:36 +0300

    refactor import

--- src/stack/MainStack.tsx
+++ src/stack/MainStack.tsx
@@ -4,14 +4,14 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { ROUTES } from '../constants/routes';
 import { MainScreen } from '../screens/mainScreen';
-import { StartScreenComponent } from '../screens/startScreen/startScreenComponent';
+import { StartScreen } from '../screens/startScreen';
 
 export const MainStackNavigator = () => {
   const Stack = createNativeStackNavigator();
 
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name={ROUTES.START_SCREEN} component={StartScreenComponent} />
+      <Stack.Screen name={ROUTES.START_SCREEN} component={StartScreen} />
       <Stack.Screen name={ROUTES.MAIN_SCREEN} component={MainScreen} />
     </Stack.Navigator>
   );
