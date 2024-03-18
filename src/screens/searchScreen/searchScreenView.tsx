42d8611bb81be6b3578e0c9f65b82695901463f2
macwolf <romazverb18@gmsil.com>
2024-03-18 23:00:56 +0300

    added insertButton

--- src/screens/searchScreen/searchScreenView.tsx
+++ src/screens/searchScreen/searchScreenView.tsx
@@ -2,26 +2,43 @@ import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 import React, { FC } from 'react';
 
+import { InsertSvg } from '../../../assets/insert';
 import { LogoComponent } from '../../components/logoComponent/logoComponent';
 
 import { searchScreenStyles } from './styles';
 import { SearchScreenProps } from './types';
 
 export const SearchScreenView: FC<SearchScreenProps> = (props) => {
-  const { value = '', setInputValue = () => {} } = props;
+  const { value = '', setInputValue = () => {}, insertDataLink = () => {} } = props;
   return (
     <View style={searchScreenStyles.rootContainer}>
       <LogoComponent />
-      <View style={{ flexDirection: 'row', alignItems: 'center', marginHorizontal: '5%' }}>
+      <View style={searchScreenStyles.inputContainer}>
         <TextInput
+          clearTextOnFocus={true}
           value={value}
           onChangeText={setInputValue}
           accessibilityLabel={'Text input field'}
           style={searchScreenStyles.input}
         />
-        <TouchableOpacity style={searchScreenStyles.buttonStyle} accessibilityLabel={'button'}>
-          <Text style={searchScreenStyles.textInput}>{'Скачать'}</Text>
-        </TouchableOpacity>
+        <View style={searchScreenStyles.buttonContainer}>
+          <TouchableOpacity
+            style={searchScreenStyles.buttonInsertStyle}
+            accessibilityLabel={'button'}
+            onChangeText={insertDataLink}
+          >
+            <View style={searchScreenStyles.buttonInsertSvgStyle}>
+              <InsertSvg />
+            </View>
+            <Text style={searchScreenStyles.textButton}>{'Вставить'}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={searchScreenStyles.buttonSaveStyle}
+            accessibilityLabel={'button'}
+          >
+            <Text style={searchScreenStyles.textButton}>{'Скачать'}</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
--- src/screens/searchScreen/styles.ts
+++ src/screens/searchScreen/styles.ts
@@ -2,30 +2,58 @@ import { StyleSheet, TextStyle, ViewStyle } from 'react-native';
 
 export type Styles = {
   rootContainer: ViewStyle;
+  inputContainer: ViewStyle;
   input: ViewStyle;
-  buttonStyle: ViewStyle;
-  textInput: TextStyle;
+  buttonSaveStyle: ViewStyle;
+  buttonInsertStyle: ViewStyle;
+  textButton: TextStyle;
+  buttonContainer: ViewStyle;
+  buttonInsertSvgStyle: ViewStyle;
 };
 
 export const searchScreenStyles = StyleSheet.create<Styles>({
   rootContainer: { backgroundColor: '#1d014b', flex: 1 },
+  inputContainer: {
+    flexDirection: 'row',
+    marginHorizontal: '5%',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: 190,
+    marginLeft: -120,
+  },
   input: {
-    width: '100%',
+    width: '80%',
     height: 50,
     padding: 10,
     borderRadius: 7,
     backgroundColor: 'white',
   },
-  buttonStyle: {
+  buttonSaveStyle: {
     width: 80,
     height: 50,
     backgroundColor: '#be54ff',
     borderRadius: 7,
     justifyContent: 'center',
     alignItems: 'center',
-    marginLeft: -80,
   },
-  textInput: {
+  buttonInsertStyle: {
+    width: 100,
+    height: 30,
+    zIndex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#191a40',
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 7,
+  },
+  buttonInsertSvgStyle: {
+    width: 30,
+    height: 18,
+  },
+  textButton: {
     color: 'white',
   },
 });
