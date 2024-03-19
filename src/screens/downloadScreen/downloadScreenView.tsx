be0a80bc69175a735cf30d15755a9ccfd8ef08a3
macwolf <romazverb18@gmsil.com>
2024-03-19 23:19:57 +0300

    added refactored downloadScreenView

--- src/screens/downloadScreen/downloadScreenView.tsx
+++ src/screens/downloadScreen/downloadScreenView.tsx
@@ -2,14 +2,20 @@ import { Text, View } from 'react-native';
 
 import React, { FC } from 'react';
 
+import { useAppSelector } from '../../hooks/hooks';
+import { getIsActive } from '../../store/homeScreen/selectors';
+
 import { downloadScreenStyles } from './styles';
 import { DownloadScreenProps } from './types';
 
 export const DownloadScreenView: FC<DownloadScreenProps> = (props) => {
   const {} = props;
+  const trueIsTrue = useAppSelector(getIsActive);
   return (
     <View style={downloadScreenStyles.rootContainer}>
-      <Text style={{ color: 'white' }}>{'Hello app'}</Text>
+      <Text style={{ color: 'white' }}>
+        {trueIsTrue === true ? ' true Hello app' : 'false Hello app'}
+      </Text>
     </View>
   );
 };
