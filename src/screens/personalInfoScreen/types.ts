f52d1e2aafc19776845ec684a3da15badb0483c6
macwolf <romazverb18@gmsil.com>
2024-03-19 23:24:21 +0300

    added new types for personalScreen

--- src/screens/personalInfoScreen/types.ts
+++ src/screens/personalInfoScreen/types.ts
@@ -1,4 +1,4 @@
-import { TouchableOpacityProps } from 'react-native';
+import { TextProps, TouchableOpacityProps } from 'react-native';
 
 import { BottomSheetModal, BottomSheetModalProps } from '@gorhom/bottom-sheet';
 
@@ -6,5 +6,8 @@ export type InfoScreenViewProps = {
   bottomSheetModalRef?: React.Ref<BottomSheetModal>;
   snapPoints: BottomSheetModalProps['snapPoints'];
   goToTermsAndConditionsModalScreen: TouchableOpacityProps['onPress'];
+  goToPrivacyPolicyModal: TouchableOpacityProps['onPress'];
+  goToContactsModal: TouchableOpacityProps['onPress'];
   goToCloseBottomSheet: TouchableOpacityProps['onPress'];
+  actionTriggered: TextProps['children'];
 };
