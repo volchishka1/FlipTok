708e6cca3a4b9d429b0c9ef9278c84af88a0e61b
macwolf <romazverb18@gmsil.com>
2024-03-19 23:21:23 +0300

    added new function to add data to redux

--- src/screens/searchScreen/index.tsx
+++ src/screens/searchScreen/index.tsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
 
+import { useAppDispatch } from '../../hooks/hooks';
+import { addIsActive } from '../../store/actions';
+
 import { SearchScreenView } from './searchScreenView';
 
 export const SearchScreen = () => {
   const [value, setValue] = useState('');
+  const dispatch = useAppDispatch();
 
   const setInputValue = (text: string) => {
-    setValue(text);
+    // setValue(text);
+    dispatch(addIsActive(true));
   };
 
   return <SearchScreenView value={value} setInputValue={setInputValue} />;
