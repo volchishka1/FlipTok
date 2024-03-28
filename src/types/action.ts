fb03728f7d88c70154b58013dfef047b4040eecc
macwolf <romazverb18@gmsil.com>
2024-03-28 12:50:48 +0300

    added new actionType

--- src/types/action.ts
+++ src/types/action.ts
@@ -5,6 +5,8 @@ import { RootState } from '../store/root-reducer';
 
 export enum ActionType {
   ChangeActive = 'user/ChangeActive',
+  SaveData = 'user/SaveData',
+  SetLink = 'user/SetLink',
 }
 
 export type ThunkActionResult<R = Promise<void | boolean>> = ThunkAction<
