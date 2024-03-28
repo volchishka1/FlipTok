534fca634941c38150e45212c4bf872414cc6351
macwolf <romazverb18@gmsil.com>
2024-03-28 12:55:04 +0300

    added new selector getLoadData

--- src/store/homeScreen/selectors.ts
+++ src/store/homeScreen/selectors.ts
@@ -1,3 +1,4 @@
 import { NameSpace, RootState } from '../root-reducer';
 
 export const getIsActive = (state: RootState): boolean => state[NameSpace.homeScreen].isActive;
+export const getLoadData = (state: RootState) => state[NameSpace.homeScreen].itemData;
