43312ea91eb3002f7a21e56a7abd9b6f9b678939
macwolf <romazverb18@gmsil.com>
2024-03-28 12:53:28 +0300

    added new useState in searchScreen

--- src/screens/searchScreen/index.tsx
+++ src/screens/searchScreen/index.tsx
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
 
-import { useAppDispatch } from '../../hooks/hooks';
+import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
 import { addIsActive } from '../../store/actions';
+import { getLoadData } from '../../store/homeScreen/selectors';
 
 import { SearchScreenView } from './searchScreenView';
 
 export const SearchScreen = () => {
-  const [value, setValue] = useState('');
+  const [link, setLink] = useState('');
+  const [text, setText] = useState('');
   const dispatch = useAppDispatch();
+  const getLoadedData = useAppSelector(getLoadData);
 
-  const setInputValue = (text: string) => {
-    // setValue(text);
+  const setInputValue = () => {
+    setText(link);
     dispatch(addIsActive(true));
   };
 
-  return <SearchScreenView value={value} setInputValue={setInputValue} />;
+  return (
+    <SearchScreenView text={text} link={link} setLink={setLink} setInputValue={setInputValue} />
+  );
 };
