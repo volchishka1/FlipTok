fcfa2781fa7fd933f116b6ccebcdfffe80ee894f
macwolf <romazverb18@gmsil.com>
2024-03-28 12:52:25 +0300

    added new types in homeScreen

--- src/store/homeScreen/homeScreen.ts
+++ src/store/homeScreen/homeScreen.ts
@@ -1,14 +1,31 @@
 import { createReducer } from '@reduxjs/toolkit';
 
 import { HomeScreen } from '../../types/state';
-import { addIsActive } from '../actions';
+import { addIsActive, saveData } from '../actions';
 
 const initialState: HomeScreen = {
   isActive: false,
+  itemData: {
+    music: {
+      id: '',
+      title: '',
+      url: '',
+    },
+    video: {
+      id: '',
+      title: '',
+      cover: '',
+      url: '',
+    },
+  },
 };
 
 export const homeScreen = createReducer(initialState, (builder) => {
-  builder.addCase(addIsActive, (state, action) => {
-    state.isActive = action.payload;
-  });
+  builder
+    .addCase(addIsActive, (state, action) => {
+      state.isActive = action.payload;
+    })
+    .addCase(saveData, (state, action) => {
+      state.itemData = action.payload;
+    });
 });
