0da7e6fa241444b3c5b483c82a3dd4ad710af0b8
macwolf <romazverb18@gmsil.com>
2024-03-28 12:55:27 +0300

    added new types in state

--- src/types/state.ts
+++ src/types/state.ts
@@ -1,3 +1,16 @@
 export type HomeScreen = {
   isActive: boolean;
+  itemData: {
+    music: {
+      id: string;
+      title: string;
+      url: string;
+    };
+    video: {
+      id: string;
+      title: string;
+      cover: string;
+      url: string;
+    };
+  };
 };
