6c0fc9bd8effa3c6b194d3689c797d043924790a
macwolf <romazverb18@gmsil.com>
2024-03-28 12:51:06 +0300

    added new action

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -5,3 +5,11 @@ import { ActionType } from '../types/action';
 export const addIsActive = createAction(ActionType.ChangeActive, (isActive: boolean) => ({
   payload: isActive,
 }));
+
+export const saveData = createAction(ActionType.SaveData, (saveData: any) => ({
+  payload: saveData,
+}));
+
+export const setLink = createAction(ActionType.SetLink, (SetLink: string) => ({
+  payload: SetLink,
+}));
